00b5a5f8c3e2552ba819c8ce5a8508e7cb0961ea
Marc España Quinquilla <meq.1515@gmail.com>
2024-09-24 17:22:55 +0200

    I'm still working with the authentication

---
+++ src/lib/auth.ts
@@ -0,0 +1,45 @@
+import { NextAuthOptions } from "next-auth";
+import { db } from "./db";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/sign-in",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.image;
+        session.user.username = token.username;
+      }
+
+      return session;
+    },
+  },
+
+  async jwt({ token, user }) {
+    const dbUser = await db.user.findFirst({
+      where: {
+        email: token.email,
+      },
+    });
+
+    if (!dbUser) {
+      token.id = user!.id;
+    }
+  },
+};
