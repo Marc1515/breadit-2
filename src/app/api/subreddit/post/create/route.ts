59f3372cd04d96065add73d5a329d94c3855bbab
Marc España Quinquilla <meq.1515@gmail.com>
2024-09-30 13:06:11 +0200

    Now is posible to create a post

--- src/app/api/subreddit/post/create/route.ts
+++ src/app/api/subreddit/post/create/route.ts
@@ -1,5 +1,6 @@
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { PostValidator } from "@/lib/validators/post";
 import { SubredditSubsctiptionValidator } from "@/lib/validators/subreddit";
 import { z } from "zod";
 
@@ -13,7 +14,7 @@ export async function POST(req: Request) {
 
     const body = await req.json();
 
-    const { subredditId } = SubredditSubsctiptionValidator.parse(body);
+    const { subredditId, title, content } = PostValidator.parse(body);
 
     const subscriptionExist = await db.subscription.findFirst({
       where: {
@@ -22,26 +23,31 @@ export async function POST(req: Request) {
       },
     });
 
-    if (subscriptionExist) {
-      return new Response("You are already subscribed to this subreddit", {
+    if (!subscriptionExist) {
+      return new Response("Subscribe to post", {
         status: 400,
       });
     }
 
-    await db.subscription.create({
+    await db.post.create({
       data: {
+        title,
+        content,
+        authorId: session.user.id,
         subredditId,
-        userId: session.user.id,
       },
     });
 
-    return new Response(subredditId);
+    return new Response("OK");
   } catch (error) {
     if (error instanceof z.ZodError) {
       return new Response("Invalid POST request data passed", { status: 422 });
     }
-    return new Response("Could not subscrive, please try again later", {
-      status: 500,
-    });
+    return new Response(
+      "Could not post to subreddit this time, please try again later",
+      {
+        status: 500,
+      }
+    );
   }
 }
