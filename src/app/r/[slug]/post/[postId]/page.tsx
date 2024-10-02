b76d7ab1de92face51a050a527fdced47d050c80
Marc España Quinquilla <meq.1515@gmail.com>
2024-10-02 19:07:47 +0200

    Working with the dynamic route of the postId

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,9 +1,12 @@
+import PostVoteServer from "@/components/post-vote/PostVoteServer";
+import { buttonVariants } from "@/components/ui/Button";
 import { db } from "@/lib/db";
 import { redis } from "@/lib/redis";
 import { CachedPost } from "@/types/redis";
 import { Post, User, Vote } from "@prisma/client";
+import { ArrowBigDown, ArrowBigUp, Loader2 } from "lucide-react";
 import { notFound } from "next/navigation";
-import { FC } from "react";
+import { FC, Suspense } from "react";
 
 interface PageProps {
   params: {
@@ -15,13 +18,13 @@ export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
 const Page = async ({ params }: PageProps) => {
-  const cachePost = (await redis.hgetall(
+  const cachedPost = (await redis.hgetall(
     `post:${params.postId}`
   )) as CachedPost;
 
   let post: (Post & { votes: Vote[]; author: User }) | null = null;
 
-  if (!cachePost) {
+  if (!cachedPost) {
     post = await db.post.findFirst({
       where: {
         id: params.postId,
@@ -33,9 +36,49 @@ const Page = async ({ params }: PageProps) => {
     });
   }
 
-  if (!post && !cachePost) return notFound();
+  if (!post && !cachedPost) return notFound();
 
-  return <div>Page</div>;
+  return (
+    <div>
+      <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between">
+        <Suspense fallback={<PostVoteShell />}>
+          {/* @ts-expect-error server component */}
+          <PostVoteServer
+            postId={post?.id ?? cachedPost.id}
+            getData={async () => {
+              return await db.post.findUnique({
+                where: {
+                  id: params.postId,
+                },
+                include: {
+                  votes: true,
+                },
+              });
+            }}
+          />
+        </Suspense>
+      </div>
+    </div>
+  );
 };
+
+function PostVoteShell() {
+  return (
+    <div className="flex items-center flex-col pr-6 w-20">
+      {/* upvote */}
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigUp className="h-5 w-5 text-zinc-700" />
+      </div>
+      {/* score */}
+      <div className="text-center py-2 font-medium text-sm text-zinc-900">
+        <Loader2 className="h-3 w-3 animate-spin" />
+      </div>
+      {/* downvote */}
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigDown className="h-5 w-5 text-zinc-700" />
+      </div>
+    </div>
+  );
+}
 
 export default Page;
